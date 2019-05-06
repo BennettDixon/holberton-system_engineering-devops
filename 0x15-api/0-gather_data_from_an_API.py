6155a08e6dfdd3db3a00d69b37478815a91855ff
BennettDixon <bennettdixon16@gmail.com>
2019-05-06 16:15:10 +0000

    using python requests library to explore example REST apis and gather their objects/information

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""script for parsing web data from an api
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+    base_url = 'https://jsonplaceholder.typicode.com/'
+    try:
+        employee_id = sys.argv[1]
+    except:
+        print('Usage: {} employee_id'.format(sys.argv[0]))
+        exit(1)
+
+    # grab the info about the user
+    url = base_url + 'users?id={}'.format(employee_id)
+    response = requests.get(url)
+    user = json.loads(response.text)
+    name = user[0].get('name')
+
+    # grab the info about the user's tasks
+    url = base_url + 'todos?userId={}'.format(employee_id)
+    response = requests.get(url)
+    objs = json.loads(response.text)
+    completed = 0
+    completed_tasks = []
+    for obj in objs:
+        if obj.get('completed'):
+            completed_tasks.append(obj)
+            completed += 1
+
+    # print the output about user's task completion
+    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
+    # print the output title of completed tasks
+    for task in completed_tasks:
+        print("\t {}".format(task.get('title')))
