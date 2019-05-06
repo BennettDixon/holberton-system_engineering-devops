78697a2a17d98d8af353ad94edbc3bffa93008b6
BennettDixon <bennettdixon16@gmail.com>
2019-05-06 16:59:47 +0000

    using rest apis to build comprehensive dynamic lists depending on results from calls to different endpoints

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""script for parsing web data from an api
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+    import json
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    # grab info about all users
+    url = base_url + 'users'
+    response = requests.get(url)
+    users = json.loads(response.text)
+
+    # grab the info about the users' tasks
+    builder = {}
+    for user in users:
+        print('grabbing info on user {}'.format(user.get('username')))
+        employee_id = user.get('id')
+        user_id_key = str(employee_id)
+        username = user.get('username')
+        builder[user_id_key] = []
+        url = base_url + 'todos?userId={}'.format(employee_id)
+
+        response = requests.get(url)
+        objs = json.loads(response.text)
+        for obj in objs:
+                json_data = {
+                    "task": obj.get('title'),
+                    "completed": obj.get('completed'),
+                    "username": username
+                }
+                builder[user_id_key].append(json_data)
+
+    # write the data to the file
+    json_encoded_data = json.dumps(builder)
+    with open('todo_all_employees.json', 'w') as myFile:
+        myFile.write(json_encoded_data)
