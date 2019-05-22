e2e19a610c816fb10be379db235b3ed871e270f8
BennettDixon <bennettdixon16@gmail.com>
2019-05-22 16:29:18 +0000

    Removed extra comments

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
 # issue on line 137 of /var/www/html/wp-settings.php
-# extra p on the class-wp-locale.php
-# remove the p and restart/reload apache
 $rep = 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );'
 $settings = '/var/www/html/wp-settings.php'
 $cmd = "sed -i \"/class-wp-locale.phpp/c ${::rep}\" ${::settings}"
