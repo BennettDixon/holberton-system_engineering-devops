e1844d36c08acc4f4dcd1435e1d0090670786bf3
BennettDixon <bennettdixon16@gmail.com>
2019-05-22 16:24:46 +0000

    fixed puppet lint errors

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,10 @@
 # issue on line 137 of /var/www/html/wp-settings.php
 # extra p on the class-wp-locale.php
 # remove the p and restart/reload apache
-exec { 'sedfix':
-  path => '/bin',
-  command => 'sed -i "/class-wp-locale.phpp/ c\require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );" /var/www/html/wp-settings.php',
-}
-
-service { 'mysql':
-  ensure => running,
+$rep = 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );'
+$settings = '/var/www/html/wp-settings.php'
+$cmd = "sed -i \"/class-wp-locale.phpp/c ${::rep}\" ${::settings}"
+exec { 'serverfix':
+  path    => '/bin',
+  command => $cmd,
 }
